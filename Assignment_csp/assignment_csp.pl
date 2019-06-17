2b280ee28a71e940ff6413d61d92e1a740e62dd4
Fanarosss <theof.aslanidis@gmail.com>
2019-06-17 22:30:13 +0300

    New implementation csp

--- Assignment_csp/assignment_csp.pl
+++ Assignment_csp/assignment_csp.pl
@@ -2,113 +2,19 @@
 % Editor: Aslanidis Theofanis
 % AM : 1115201500013
 % Used Eclipse for the implementation
+% !!! Ta ilopoiisa ola apla eixa ena thema ston elegxo na min kanoun interfere oi xronoi
+% tou idiou atoma, exo ftiakei ti sinartisi kai exo vgalei kai tipota gia to pote kanoun
+% interfere. T01 + T02 - (T11 + T12) pairno to absolute autis tis diaforas kai afairw to maximum
+% apo tis dio diafores T01-T02 kai T11+T12, opote o vazw oles tis ic metavlites me tis times se mia lista
+% kai thelw to min tis listas na einai megalitero tou 0, ama einai mikrotero, simainei oti 2 activities kanoun
+% overlap stous xronous tous.
+
+% ola ta ipoloipa mou doulevoun kala.
+% Exo xrisimopoiisei reified constraints, exontas to S poy exei gia kathe activity poios tin exei
+% kai to CurrPersonsActList poy pernei [0,1] times kai einai gia kathe anthropo, poia activity exei parei.
 
 :- lib(ic).
 :- lib(branch_and_bound).
-:- lib(listut).
-
-% Knowledge Base:
-activity(a001, act(41,49)).
-activity(a002, act(72,73)).
-activity(a003, act(80,85)).
-activity(a004, act(65,74)).
-activity(a005, act(96,101)).
-activity(a006, act(49,55)).
-activity(a007, act(51,59)).
-activity(a008, act(63,65)).
-activity(a009, act(66,69)).
-activity(a010, act(80,87)).
-activity(a011, act(71,76)).
-activity(a012, act(64,68)).
-activity(a013, act(90,93)).
-activity(a014, act(49,56)).
-activity(a015, act(23,29)).
-activity(a016, act(94,101)).
-activity(a017, act(25,34)).
-activity(a018, act(51,54)).
-activity(a019, act(13,23)).
-activity(a020, act(67,72)).
-activity(a021, act(19,21)).
-activity(a022, act(12,16)).
-activity(a023, act(99,104)).
-activity(a024, act(92,94)).
-activity(a025, act(74,83)).
-activity(a026, act(95,100)).
-activity(a027, act(39,47)).
-activity(a028, act(39,49)).
-activity(a029, act(37,39)).
-activity(a030, act(57,66)).
-activity(a031, act(95,101)).
-activity(a032, act(71,74)).
-activity(a033, act(86,93)).
-activity(a034, act(51,54)).
-activity(a035, act(74,83)).
-activity(a036, act(75,81)).
-activity(a037, act(33,43)).
-activity(a038, act(29,30)).
-activity(a039, act(58,60)).
-activity(a040, act(52,61)).
-activity(a041, act(35,39)).
-activity(a042, act(46,51)).
-activity(a043, act(71,72)).
-activity(a044, act(17,24)).
-activity(a045, act(94,103)).
-activity(a046, act(77,87)).
-activity(a047, act(83,87)).
-activity(a048, act(83,92)).
-activity(a049, act(59,62)).
-activity(a050, act(2,4)).
-activity(a051, act(86,92)).
-activity(a052, act(94,103)).
-activity(a053, act(80,81)).
-activity(a054, act(39,46)).
-activity(a055, act(60,67)).
-activity(a056, act(72,78)).
-activity(a057, act(58,61)).
-activity(a058, act(8,18)).
-activity(a059, act(12,16)).
-activity(a060, act(47,50)).
-activity(a061, act(49,50)).
-activity(a062, act(71,78)).
-activity(a063, act(34,42)).
-activity(a064, act(21,26)).
-activity(a065, act(92,95)).
-activity(a066, act(80,81)).
-activity(a067, act(74,79)).
-activity(a068, act(28,29)).
-activity(a069, act(100,102)).
-activity(a070, act(29,37)).
-activity(a071, act(4,12)).
-activity(a072, act(79,83)).
-activity(a073, act(98,108)).
-activity(a074, act(91,100)).
-activity(a075, act(82,91)).
-activity(a076, act(59,66)).
-activity(a077, act(34,35)).
-activity(a078, act(51,60)).
-activity(a079, act(92,94)).
-activity(a080, act(77,83)).
-activity(a081, act(38,48)).
-activity(a082, act(51,59)).
-activity(a083, act(35,39)).
-activity(a084, act(22,24)).
-activity(a085, act(67,68)).
-activity(a086, act(90,97)).
-activity(a087, act(82,83)).
-activity(a088, act(51,53)).
-activity(a089, act(78,88)).
-activity(a090, act(74,79)).
-activity(a091, act(100,105)).
-activity(a092, act(53,63)).
-activity(a093, act(57,66)).
-activity(a094, act(32,41)).
-activity(a095, act(48,56)).
-activity(a096, act(92,96)).
-activity(a097, act(4,8)).
-activity(a098, act(31,33)).
-activity(a099, act(69,77)).
-activity(a100, act(88,93)).
-
 
 %arguments: -CP|RP- List to recurse for all players
 %           -InitasP- Empty List
@@ -172,44 +78,71 @@ between(X1,X2,X) :-
   X1 < X2, NewX1 is X1 + 1,
   between(NewX1, X2, X).
 
-% custom sum
-my_sum(_, [], [], Sol, L, 0).
-my_sum(A, [X|S], [ActId|RestActivities], Sol, L, Cost) :-
-  my_sum(A, S, RestActivities, Sol, L, CurrCost),
+compute_st(_, [], [], 0).
+compute_st(A, [X|ActList], [ActId|RestActivities], ST) :-
+  compute_st(A, ActList, RestActivities, RecST),
   activity(ActId, act(T01, T02)),
-  compute_st(X, Sol, L, T01, T02, 0, Wi),
-  Cost #= eval(CurrCost + (A - Wi)*(A - Wi)).
+  ActTime is T02 - T01,
+  ST #= eval(RecST + X * ActTime).
+
+% CostList
+my_sum(_, _, _, [], []).
+my_sum(A, S, L, [CPacts|ActLists], [ST|CostList]) :-
+  compute_st(A, CPacts, L, Wi),
+  ST #= sqr(eval((A-Wi))),
+  my_sum(A, S, L, ActLists, CostList).
+
+initCPList(_, [], []).
+initCPList(CurrPerson, [CPAct|RestList], [CP|S]) :-
+  initCPList(CurrPerson, RestList, S),
+  CP #\= CurrPerson => CPAct #= 0.
+
+time_max(X,Y,X) :- X >= Y, !.
+time_max(_,Y,Y).
+
+absolute(X, Y) :- X < 0, Y is -1*X, !.
+absolute(X, X).
 
 
-compute_st(_, [], [], _, _, ST, ST).
-compute_st(X, [CP|S], [ActId|L], T01, T02, CurrST, ST) :-
-  get_domain_size(CP,Size),
-  Size == 1,
-  X #= CP,
+apply_constraint(_, [], [], _, _, []).
+apply_constraint(CPAct, [CPAct2|RestList], [ActId|L], T01, T02, [P|Overlapping]) :-
   activity(ActId, act(T11, T12)),
-  NewST is CurrST + T12 - T11,
-  compute_st(X, S, L, T01, T02, NewST, ST).
-compute_st(X, [CP|S], [ActId|L], T01, T02, CurrST, ST) :-
-  get_domain_size(CP,Size),
-  Size == 1,
-  X #\= CP,
-  compute_st(X, S, L, T01, T02, CurrST, ST).
-compute_st(X, [CP|S], [ActId|L], T01, T02, CurrST, ST) :-
-  get_domain_size(CP,Size),
-  Size > 1,
-  compute_st(X, S, L, T01, T02, CurrST, ST).
+  ActTime1 is T02 - T01,
+  ActTime2 is T12 - T11,
+  time_max(ActTime1, ActTime2, Max),
+  Computation is T01 + T02 - T11 - T12,
+  AbsoluteV is absolute(Computation),
+  P #= eval(CPAct*CPAct2*(AbsoluteV-Max)),
+  apply_constraint(CPAct, RestList, L, T01, T02, Overlapping).
 
-test(X, ST, ActId, S, L) :-
+constraint2(_, [], [], [], _, _).
+constraint2(CurrPerson, [CPAct|RestList], [CP|S], [ActId|L], RecCPAs, ActList) :-
+  constraint2(CurrPerson, RestList, S, L, RecCPAs, ActList),
   activity(ActId, act(T01, T02)),
-  %compute current time of person
-  compute_st(X, S, L, T01, T02, 0, CurrST),
-  CurrST #=< ST.
+  apply_constraint(CPAct, RecCPAs, ActList, T01, T02, OverlappingList),
+  min(OverlappingList) #= 0.
 
-constraints([], _, [], _, _).
-constraints([X|S], ST, [ActId|L], Sol, ActList) :-
-  indomain(X),
-  test(X, ST, ActId, Sol, ActList),
-  constraints(S, ST, L, Sol, ActList).
+
+constraint1(_, [], [], [], 0).
+constraint1(CurrPerson, [CPAct|RestList], [CP|S], [ActId|L], Sum) :-
+  constraint1(CurrPerson, RestList, S, L, CurrSum),
+  activity(ActId, act(T01, T02)),
+  CP #= CurrPerson => CPAct #= 1,
+  ActTime is T02 - T01,
+  Sum #= eval(CurrSum + CPAct*ActTime).
+
+constraints([], _, _, _, _).
+constraints([CurrPerson|RestPersons], S, ST, L, [CurrPersonsActList|ActLists]) :-
+  length(L, ActivityNum),
+  length(CurrPersonsActList, ActivityNum),
+  % a list with the activities that this person has. 0 for not having, 1 for having.
+  CurrPersonsActList #:: 0..1,
+  initCPList(CurrPerson, CurrPersonsActList, S),
+  constraint1(CurrPerson, CurrPersonsActList, S, L, Sum),
+  Sum #=< ST,
+  % I had a problem in this function so I commented it.
+  %constraint2(CurrPerson, CurrPersonsActList, S, L, CurrPersonsActList, L),
+  constraints(RestPersons, S, ST, L, ActLists).
 
 
 assignment_csp(NP, ST, ASP, ASA) :-
@@ -219,16 +152,16 @@ assignment_csp(NP, ST, ASP, ASA) :-
   length(L,ActivityNum),
   findall(X, between(1, NP, X), ListOfNP),
   initASP(ListOfNP, [], [], InitASP, Persons_ST),
-  length(S, ActivityNum), % variable logical domain
+  length(S, ActivityNum),
   S #:: 1..NP,
   % constraints:
   %   - person work time < ST
   %   - no activities interfering
-  constraints(S, ST, L, S, L),
-  search(S, 0, occurence, indomain_middle, complete, []),
+  constraints(ListOfNP, S, ST, L, ActLists),
+  search(S, 0, first_fail, indomain, complete, []),
   convert_solution_to_ASP(S, L, InitASP, ASP),
   convert_solution_to_ASA(S, L, ASA).
-  % convert solution to asa and exit
+  % convert solution to asp and asa and exit
 
 assignment_opt(NF, NP, ST, F, T, ASP, ASA, Cost) :-
   % Findall(X, between(), L) gia na ftiaksw ti lista me ta activity
@@ -241,12 +174,16 @@ assignment_opt(NF, NP, ST, F, T, ASP, ASA, Cost) :-
   length(L,ActivityNum),
   findall(X, between(1, NP, X), ListOfNP),
   initASP(ListOfNP, [], [], InitASP, Persons_ST),
-  length(S, ActivityNum), % variable logical domain
+  length(S, ActivityNum),
   S #:: 1..NP,
   % constraints:
   %   - person work time < ST
   %   - no activities interfering
-  bb_min((constraints(S, ST, L, S, L), my_sum(A, S, L, S, L, Cost)), Cost, bb_options{strategy:restart, factor:F, from: 0, timeout:T}),
+  constraints(ListOfNP, S, ST, L, ActLists),
+  my_sum(A, S, L, ActLists, CostList),
+  Cost #= sum(CostList),
+  write(Cost),nl,
+  bb_min(search(S, 0, first_fail, indomain, complete, []), Cost, bb_options{strategy:restart, factor:F, timeout:T}),
   convert_solution_to_ASP(S, L, InitASP, ASP),
   convert_solution_to_ASA(S, L, ASA).
   % convert solution to asa and exit
